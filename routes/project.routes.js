44d004066286a820cd9036acaa11573ae4a80739
“Alexandre <coma.alexandre@gmail.com>
2022-08-09 10:03:44 +0200

    initial commit

---
+++ routes/project.routes.js
@@ -0,0 +1,72 @@
+const router = require("express").Router();
+const mongoose = require('mongoose');
+const Project = require('../models/Project.model');
+const Task = require('../models/Task.model');
+const {isAuthenticated} = require('../middleware/jwt.middleware')
+
+// GET list of projects
+router.get('/projects', (req, res, next) => {
+    Project.find()
+        .populate('tasks')
+        .then(allProjects => res.json(allProjects))
+        .catch(err => res.json(err));
+});
+
+//  POST /api/projects  -  Creates a new project
+router.post('/projects', isAuthenticated, (req, res, next) => {
+  const { title, description } = req.body;
+
+  Project.create({ title, description, tasks: [] })
+    .then(response => res.json(response))
+    .catch(err => res.json(err));
+});
+
+//READ project details
+router.get('/projects/:projectId', (req, res, next) => {
+    const { projectId } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(projectId)) {
+        res.status(400).json({ message: 'Specified id is not valid' });
+        return;
+    }
+
+    // Each Project document has `tasks` array holding `_id`s of Task documents
+    // We use .populate() method to get swap the `_id`s for the actual Task documents
+    Project.findById(projectId)
+        .populate('tasks')
+        .then(project => res.json(project))
+        .catch(error => res.json(error));
+});
+
+//UPDATE project
+router.put('/projects/:projectId', (req, res, next) => {
+    const { projectId } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(projectId)) {
+        res.status(400).json({ message: 'Specified id is not valid' });
+        return;
+    }
+
+    Project.findByIdAndUpdate(projectId, req.body, { new: true })
+        .then((updatedProject) => res.json(updatedProject))
+        .catch(error => res.json(error));
+});
+
+//DELETE project
+router.delete('/projects/:projectId', (req, res, next) => {
+    const { projectId } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(projectId)) {
+        res.status(400).json({ message: 'Specified id is not valid' });
+        return;
+    }
+
+    Project.findByIdAndRemove(projectId)
+        .then(deteletedProject => {
+            return Task.deleteMany({ _id: { $in: deteletedProject.tasks } });
+        })
+        .then(() => res.json({ message: `Project with id ${projectId} & all associated tasks were removed successfully.` }))
+        .catch(error => res.status(500).json(error));
+});
+
+module.exports = router;
